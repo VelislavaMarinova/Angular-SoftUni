ceb5154b27480f7c44761bd98daf00e80827dd6f
Vel MAr <velmar.annabell@gmail.com>
2023-07-16 18:33:19 +0300

    add reactive forms on register, add profile page

--- 06. Workshop Modules and Routing/workshop-modules-and-routing/src/app/theme/theme.module.ts
+++ 06. Workshop Modules and Routing/workshop-modules-and-routing/src/app/theme/theme.module.ts
@@ -4,6 +4,7 @@ import { NewThemeComponent } from './new-theme/new-theme.component';
 import { CurrentThemeComponent } from './current-theme/current-theme.component';
 import { ThemeRoutingModule } from './theme-routing.module';
 import { WelcomeComponent } from '../welcome/welcome.component';
+import { FormsModule } from '@angular/forms'
 
 
 
@@ -11,12 +12,12 @@ import { WelcomeComponent } from '../welcome/welcome.component';
   declarations: [
     NewThemeComponent,
     CurrentThemeComponent,
-  
+
   ],
   imports: [
     CommonModule,
     ThemeRoutingModule,
- 
+    FormsModule,
   ]
 })
 export class ThemeModule { }
--- 06. Workshop Modules and Routing/workshop-modules-and-routing/src/app/user/register/register.component.ts
+++ 06. Workshop Modules and Routing/workshop-modules-and-routing/src/app/user/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { DEFAULT_EMAIL_DOMAINS } from 'src/app/shared/constants';
+import { appEmailValidator } from 'src/app/shared/validators/app-email-validator';
+import { matchPassValidator } from 'src/app/shared/validators/match-password-validator';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +10,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  form = this.fb.group({
+    username: ['',[Validators.required, Validators.minLength(5)]],
+    email: ['',[Validators.required, appEmailValidator(DEFAULT_EMAIL_DOMAINS)]],
+    tel: [''],
+    passGroup: this.fb.group({
+      password: ['',[Validators.required, Validators.minLength(5)]],
+      rePassword: ['',[Validators.required]],
+    },
+    {
+      validators:[matchPassValidator('password','rePassword')]
+    })
+  });
+
+  constructor(private fb: FormBuilder) { }
+
+  register(): void {
+    if(this.form.invalid){
+      return;
+    }
+    console.log(this.form.value);
+
+  }
 
 }
